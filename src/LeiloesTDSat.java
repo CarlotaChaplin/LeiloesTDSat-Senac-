e21867556f487454ac6b6c9e74c03e32a967b033
Carlota Chaplin <carlotachaplin@gmail.com>
2024-02-23 15:47:41 -0300

    Criação da class main

---
+++ src/LeiloesTDSat.java
@@ -0,0 +1,10 @@
+
+public class LeiloesTDSat {
+   public static void main(String[]args){
+        
+        cadastroVIEW telaInicial = new cadastroVIEW();
+        
+        telaInicial.setVisible(true);
+        
+    } 
+}
